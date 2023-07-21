0e4a3eb75f473bc925f64e6fb6cac78ba68de390
L-df <bhlamarche@gmail.com>
2023-07-21 22:57:01 +0000

    Creating glfw window

--- GLFW/main.cpp
+++ GLFW/main.cpp
@@ -5,6 +5,17 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+
+void GLFWErrorCallback(int errorNumber, const char* errorDescription)
+{
+	fprintf(stderr, "GLFW Error: %s\n", errorDescription);
+	return;
+}
+
+static const int WindowWidth = 640;
+static const int WindowHeight = 480;
+static const char WindowTitle[100] = "Example Window Title\0";
+
 int main(void)
 {
 	if(!glfwInit())
@@ -12,8 +23,14 @@ int main(void)
 		fprintf(stderr, "Failed to initialize glfw\n");
 		return 1;
 	}
-	
+	glfwSetErrorCallback(GLFWErrorCallback);
 
+	GLFWwindow* window = glfwCreateWindow(WindowWidth, WindowHeight, WindowTitle, NULL, NULL);
+
+	if(!window)
+	{
+		fprintf(stderr, "Failed to create glfw window\n");
+	}
 
 	glfwTerminate();
 	return 0;
