d0b92bf7b82e68d828cc03fd4ebdfa20e740e535
L-df <bhlamarche@gmail.com>
2023-08-12 00:23:54 +0000

    Tried drawing circle, got rotated square instead, committing because I don't get much time to work on personal projects

--- GLFW/main.cpp
+++ GLFW/main.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GLFW/glfw3.h>
-
+#include <math.h>
 
 
 struct GLFWPixel
 {
+public:
     unsigned char r = 0;
     unsigned char g = 0;
     unsigned char b = 0;
@@ -69,9 +70,10 @@ namespace GLFWCallbacksAndInitialization
 template<int Width, int Height>
 struct SimplePixelArray
 {
-    GLFWPixel PixelData[Width*Height];
+public:
+    GLFWPixel PixelData[Width*Height] = {};
 
-    GLFWPixel& GetPixel(int x, int y) { return PixelData[x*Width + y]; };
+    constexpr GLFWPixel& GetPixel(int x, int y) { return PixelData[x*Width + y]; };
 };
 
 template<int Width, int Height>
@@ -84,6 +86,62 @@ constexpr SimplePixelArray<Width, Height> GenerateSimpleColor(int r, int g, int 
         ReturnArray.PixelData[index].r = r;
         ReturnArray.PixelData[index].g = g;
         ReturnArray.PixelData[index].b = b;
+    }
+
+    return ReturnArray;
+}
+
+template<typename NumericType = int>
+struct Vector2
+{
+    NumericType X;
+    NumericType Y;
+};
+
+struct Sphere
+{
+    union
+    {
+        Vector2<int> Center;
+        int C[2];
+    };
+    int Radius;
+};
+
+constexpr Sphere CreateSphere(int CenterX, int CenterY, int Radius)
+{
+    Sphere ReturnObject = Sphere();
+
+    ReturnObject.Center.X = CenterX;
+    ReturnObject.Center.Y = CenterY;
+    ReturnObject.Radius = Radius;
+    
+   return ReturnObject;
+}
+
+template<int Width, int Height>
+constexpr SimplePixelArray<Width, Height> 
+    GenerateSimpleColor(
+    const int& r, const int& g, const int& b,
+    const SimplePixelArray<Width, Height>& BaseImageToChange, 
+    const Sphere& SphereToDraw)
+{
+    SimplePixelArray<Width, Height> ReturnArray = BaseImageToChange;
+
+    for(int HeightIndex = 0; HeightIndex < Height; HeightIndex++)
+    {
+        int HeightDifference = std::abs(HeightIndex - SphereToDraw.Center.Y);
+        
+        for(int WidthIndex = 0; WidthIndex < Width; WidthIndex++)
+        {
+            int WidthDifference = std::abs(WidthIndex - SphereToDraw.Center.X);
+            if(HeightDifference + WidthDifference < SphereToDraw.Radius)
+            {
+                ReturnArray.GetPixel(WidthIndex, HeightIndex).r = r;
+                ReturnArray.GetPixel(WidthIndex, HeightIndex).g = g;
+                ReturnArray.GetPixel(WidthIndex, HeightIndex).b = b;
+            }
+        }
     }
 
     return ReturnArray;
@@ -102,8 +160,11 @@ int main(void)
     // TODO: Do the thing in linux that removes the command line if run by file explorer
     fprintf(stdout, "Press Escape to close\n");    
 
-    constexpr SimplePixelArray<WindowWidth, WindowHeight> DisplayPixels =
+    constexpr SimplePixelArray<WindowWidth, WindowHeight> OriginalDisplayPixels =
              GenerateSimpleColor<WindowWidth, WindowHeight>(255, 255, 255);    
+
+    constexpr Sphere SphereToDrawOne = CreateSphere(WindowWidth * 0.33, WindowHeight * 0.5, WindowWidth * 0.1);
+    
 
 	while(window && !glfwWindowShouldClose(window))
 	{
@@ -116,6 +177,10 @@ int main(void)
         //        https://www.glfw.org/GLFWReference27.pdf
         //        Looking at the table of contents        
         // TODO: Just skip directly to VULKAN and remove -lGL from build script
+    
+        constexpr SimplePixelArray<WindowWidth, WindowHeight> DisplayPixels 
+            = GenerateSimpleColor<WindowWidth, WindowHeight>(125, 125, 125, OriginalDisplayPixels, SphereToDrawOne);        
+
         glDrawPixels(WindowWidth, WindowHeight, GL_RGB, GL_UNSIGNED_BYTE, &DisplayPixels.PixelData);
 	
 		glfwSwapBuffers(window);
