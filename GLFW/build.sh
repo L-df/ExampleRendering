e39bd3db6e25f310f4303499262cc968847ec6dc
L-df <bhlamarche@gmail.com>
2023-08-13 19:00:30 +0000

    Removing verbose printing from the not debug .exe

--- GLFW/build.sh
+++ GLFW/build.sh
@@ -1,6 +1,8 @@
 !/usr/bin/env bash
 
-pushd GLFW/
-g++ -g -fconstexpr-loop-limit=400000 main.cpp DrawingPixels.h -o ExampleWindow.out -lglfw -lGL -lvulkan
-g++ -g -fconstexpr-loop-limit=400000 -O0 main.cpp DrawingPixels.h -o DebugExampleWindow.out -lglfw -lGL -lvulkan
+pushd ./GLFW/
+
+g++ -g -fconstexpr-loop-limit=400000 -DVULKAN_SEVERITY_THRESHOLD=VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT main.cpp DrawingPixels.h -o ExampleWindow.out -lglfw -lGL -lvulkan
+
+g++ -g -fconstexpr-loop-limit=400000 -O0 -DPRINT_AVAILABLE_VULKAN_EXTENSIONS_AND_LAYERS=1 main.cpp DrawingPixels.h -o DebugExampleWindow.out -lglfw -lGL -lvulkan
 popd
